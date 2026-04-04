5f0d8f12e2bdf14e4165e78bb35c83ea203db3ed
42anjakob <anjakob@student.42heilbronn.de>
2026-04-04 21:28:38 +0200

    Updated for new subject

--- py00/ex3/ft_harvest_total.py
+++ py00/ex3/ft_harvest_total.py
@@ -2,4 +2,4 @@ def ft_harvest_total() -> None:
     day1 = int(input("Day 1 harvest: "))
     day2 = int(input("Day 2 harvest: "))
     day3 = int(input("Day 3 harvest: "))
-    print(day1 + day2 + day3)
+    print(f"{day1 + day2 + day3}")
--- py00/ex6/ft_garden_summary.py
+++
@@ -1,6 +0,0 @@
-def ft_garden_summary() -> None:
-    garden_name = input("Enter garden name: ")
-    number_of_plants = int(input("Enter number of plants: "))
-    print("Garden:", garden_name)
-    print("Plants:", number_of_plants)
-    print("Status: Growing well!")
